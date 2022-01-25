2e7f830c9ac85bbdd61dd391cf50056cc98e7985
gary_s <smithgd34@gmail.com>
2022-01-25 14:12:46 -0600

    completed update exercises

---
+++ update_exercises.sql
@@ -0,0 +1,20 @@
+use codeup_test_db;
+
+select * from albums;
+update albums
+set sales = sales * 10
+where sales = sales;
+select * from albums;
+
+select name from albums where release_date < 1980;
+update albums
+set release_date = release_date - 100
+where release_date < 1980;
+select name from albums where release_date < 1980;
+select release_date from albums where release_date < 1980;
+
+select name and artist from albums where artist = 'Michael Jackson';
+update albums
+set artist = 'Peter Jackson'
+where artist = 'Michael Jackson';
+select artist, name from albums where artist = 'Peter Jackson';
