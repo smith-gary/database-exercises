9208dc43cbf273f890cfeacb0c743f1782293ce5
gary_s <smithgd34@gmail.com>
2022-01-26 09:14:59 -0600

    refactored first select statement to show a different way to put it

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,6 @@
 use codeup_test_db;
 
-select 'The name of all albums by Pink Floyd.' as 'Album Names';
-select name from albums where artist = 'Pink Floyd';
+select name as 'Pink Floyd album names' from albums where artist = 'Pink Floyd';
 
 select 'The year Sgt. Pepper''s Lonely Hearts Club Band was released.' as 'Year Released';
 select release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
@@ -13,7 +12,7 @@ select 'Which albums were released in the 1990s.' as '90''s Jams';
 select name from albums where release_date between 1990 and 2000;
 
 select 'Which albums had less than 20 million certified sales.' as 'Less than 20 mil sold';
-select name from albums where sales < 20;
-
-select 'All the albums with a genre of "Rock".' as 'Genre Rock';
-select name from albums where genre = 'Rock';
+-- select name from albums where sales < 20;
+--
+-- select 'All the albums with a genre of "Rock".' as 'Genre Rock';
+-- select name from albums where genre = 'Rock';
