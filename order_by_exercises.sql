92a0b83602a36185d463d0f1621d1a755c9e0bb7
gary_s <smithgd34@gmail.com>
2022-01-27 09:50:47 -0600

    completed order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,51 @@
+use employees;
+
+select first_name, last_name
+from employees
+where first_name in ('irena', 'vidya', 'maya')
+order by first_name asc;
+
+select first_name, last_name
+from employees
+where first_name in ('irena', 'vidya', 'maya')
+order by first_name asc, last_name asc;
+
+select first_name, last_name
+from employees
+where first_name in ('irena', 'vidya', 'maya')
+order by last_name asc, first_name asc;
+
+select last_name, emp_no
+from employees
+where last_name like ('e%')
+order by emp_no desc;
+
+select last_name, emp_no
+from employees
+where last_name like ('e%')
+   or last_name like ('%e')
+    order by emp_no desc;
+
+select last_name, emp_no
+from employees
+where last_name like ('e%')
+  and last_name like ('%e')
+    order by emp_no desc;
+
+select last_name, emp_no
+from employees
+where last_name like ('e%')
+order by emp_no asc;
+
+select last_name, emp_no
+from employees
+where last_name like ('e%')
+   or last_name like ('%e')
+    order by emp_no asc;
+
+select last_name, emp_no
+from employees
+where last_name like ('e%')
+  and last_name like ('%e')
+    order by emp_no asc;
+
